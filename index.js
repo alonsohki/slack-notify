8387ca8a3b8560b0e711e8136046b6672ca548f1
Alberto Alonso <alberto.alonso@unity3d.com>
2021-04-30 15:50:43 +0300

    Print the received content

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const url = core.getInput('slack-webhook-url');
 const content = JSON.parse(core.getInput('content'));
 const webhook = new IncomingWebhook(url);
 
+console.log(content);
+
 // Send the notification
 (async () => {
   await webhook.send(content);
