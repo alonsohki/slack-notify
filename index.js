896e47c09cebc98eb36dce02c55a7b8c515c8195
Alberto Alonso <alberto.alonso@unity3d.com>
2021-04-30 15:35:57 +0300

    Add missing require

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core');
+const { IncomingWebhook } = require('@slack/webhook');
 
 const url = core.getInput('slack-webhook-url');
 const webhook = new IncomingWebhook(url);
